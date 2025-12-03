eefe64c86a1054299b68ac5b140e0e2268a99aaa
Julien Perret <julien.perret@gmail.com>
2025-12-03 22:53:50 +0100

    adding systematic keys to components

--- src/components/News.js
+++ src/components/News.js
@@ -8,11 +8,11 @@ import {
 
 export const NewsArticle = ({ id, date, team, text }) => {
   return (
-    <div key={id}>
+    <div key={`${id}-news-article`}>
       <NewsArticleDate team={team} key={`${id}-date`}>
         <b>{date}</b>
       </NewsArticleDate>
-      <div key={`${id}-sep`}></div>
+      <div key={`${id}-sep`} />
       <NewsArticleContent key={`${id}-content`}>
         <span dangerouslySetInnerHTML={{ __html: text }} />
       </NewsArticleContent>
@@ -23,18 +23,19 @@ export const News = ({ data }) => {
   const { locale } = useLocalization();
 
   return (
-    <div>
-      <h3>{locale === "en" ? "News" : "Actualités"}</h3>
+    <div key="News">
+      <h3 key="News-title">{locale === "en" ? "News" : "Actualités"}</h3>
       <NewsList>
-        {data.allNewsCsv.nodes.map((node) => (
+        {data.allNewsCsv.nodes.map((node) =>
           // <article key={node.id} dangerouslySetInnerHTML={{ __html: `<b>${node.date} [${node.team}]:</b> ${(locale === 'en') ? node.texten : node.textfr}` }}></article>
           <NewsArticle
             id={node.id}
             date={node.date}
             team={node.team}
             text={locale === "en" ? node.texten : node.textfr}
+            key={node.id}
           />
-        ))}
+        )}
       </NewsList>
     </div>
   );
